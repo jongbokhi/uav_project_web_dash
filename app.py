f75b005f31e1228ab549c929c96e1cef9371dc91
Jongbok <105177081+jongbokhi@users.noreply.github.com>
2024-04-15 20:32:34 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ px.defaults.template = "ggplot2"
 external_css = ["https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css", ]
 
 app = Dash(__name__, pages_folder='pages', use_pages=True, external_stylesheets=external_css)
-server = app.server
+
 app.layout = html.Div([
 	html.Br(),
 	html.P('Case Presentation', className="text-dark text-center fw-bold fs-1"),
